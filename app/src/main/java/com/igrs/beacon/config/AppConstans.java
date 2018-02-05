8081a7251d23edee8993088c7101dd1a31904240
jianwuch <chjianwu@gmail.com>
2018-02-05 18:11:52 +0800

    1.密码12位

--- app/src/main/java/com/igrs/beacon/config/AppConstans.java
+++ app/src/main/java/com/igrs/beacon/config/AppConstans.java
@@ -6,7 +6,7 @@ package com.igrs.beacon.config;
 
 public class AppConstans {
 
-    public static final String DEFAULT_PASSWORD = "123456";
+    public static final String DEFAULT_PASSWORD = "147852369000";
     public static final String[] BLE_TX_POWER_LIST = {
             "0x570900:-21db", "0x570901:18db", "0x570902:-16db", "0x570903:-12db", "0x570904:-9db",
             "0x570905:-6db", "0x570906:-3db", "0x570907:0db", "0x570908:1db", "0x570909:2db",
